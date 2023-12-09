02d97c71c4105e4e1da06d1eae2aec000dd9fa86
JeetKini <151983803+JeetKini@users.noreply.github.com>
2023-12-09 18:26:44 +0530

    Add files via upload

---
+++ Week_2/Day13/Week2_day6/Week2_day6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_day6
+{
+    internal class Program
+    {
+        protected Program()
+        {
+            Console.WriteLine("hii");
+
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();   
+
+        }
+    }
+}
